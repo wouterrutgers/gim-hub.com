dbe8f2d95e1b304a8ea6bb367c1d3e8a0daeee65
Wouter Rutgers <git@wouterrutgers.nl>
2026-01-13 19:46:33 +0100

    add caching to ge prices

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\UnauthedController;
 use App\Http\Middleware\AuthenticateGroup;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ge-prices', [UnauthedController::class, 'getGEPrices']);
+Route::get('/ge-prices.json', [UnauthedController::class, 'getGEPrices']);
 Route::get('/collection-log-info', [UnauthedController::class, 'collectionLogInfo']);
 
 Route::post('/create-group', [GroupController::class, 'createGroup']);

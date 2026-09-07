255f7727428da985f6fa4e1cf8e945c6f6ec8da3
Wouter Rutgers <git@wouterrutgers.nl>
2026-09-07 17:50:07 +0200

    run styles

--- app/Models/SkillStat.php
+++ app/Models/SkillStat.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use InvalidArgumentException;
 
 class SkillStat extends Model
 {
